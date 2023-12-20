6f1ca6c833034b5d8d497e3daa2dfebec1cd870e
Hoang Phuc Seiza <hoangphucseiza@gmail.com>
2023-12-21 03:55:20 +0700

    Call video

--- client/src/components/message/CallModal.js
+++ client/src/components/message/CallModal.js
@@ -1,15 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect , useRef} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { GLOBAL_TYPES } from "../../redux/actions/globalTypes";
 
 import Avatar from "../Avatar";
 
 const CallModal = () => {
-  const { call, auth, socket } = useSelector((state) => state);
+  const { call, auth, socket, peer } = useSelector((state) => state);
+  const [hours, setHours] = useState(0)
   const [mins, setMins] = useState(0)
   const [second, setSecond] = useState(0)
   const [total, setTotal] = useState(0)
   const [answer, setAnswer] = useState(false)
+  const youVideo = useRef()
+  const otherVideo = useRef()
+  const [tracks, setTracks] = useState(null)
   const dispatch = useDispatch();
 
   // Set Time
@@ -25,6 +29,8 @@ const CallModal = () => {
   useEffect(() => {
     setSecond(total%60)
     setMins(parseInt(total/60))
+    setHours(parseInt(total/3600))
+
   }, [total])
 
   useEffect(() => { 
@@ -39,11 +45,68 @@ const CallModal = () => {
    }
   }, [answer, dispatch])
 
-  const handleAnswer = () => {
-    setAnswer(true)
+  
+  // Stream Media
+  const openStream = (video) =>{
+    const config = {audio: true, video}
+    return navigator.mediaDevices.getUserMedia(config)
   }
-    // End Call
-    const handeEndCall = () => {
+  
+  const handleAnswer = () => {
+    openStream(call.video).then(stream => {
+      playStream(youVideo.current, stream)
+      const track = stream.getTracks()
+      setTracks(track)
+
+      const newCall = peer.call(call.peerId, stream);
+      newCall.on('stream', (remoteStream) => {
+        playStream(otherVideo.current, remoteStream)
+      })
+
+      setAnswer(true)
+    })
+  }
+
+  const playStream = (tag, stream) =>{
+    let video = tag
+    video.srcObject = stream
+    video.pause()
+    video.load()
+    video.oncanplaythrough = () => {
+      if (video.readyState >= 2) {
+        video.play()
+      }
+    }
+  } 
+
+  useEffect(() => {
+    peer?.on('call', newCall => {
+      openStream(call?.video).then(stream => {
+        if(youVideo.current){
+          youVideo.current.pause();
+          playStream(youVideo.current, stream)
+        }
+        const track = stream.getTracks()
+        setTracks(track)
+
+        newCall.answer(stream);
+
+        newCall?.on('stream', (remoteStream) => {
+          if(otherVideo.current){
+            otherVideo.current.pause();
+            playStream(otherVideo.current, remoteStream)
+          }
+        });
+        setAnswer(true)
+      })
+    })
+    return () => peer?.removeListener('call');
+}, [peer, call?.video]);
+
+
+
+  // End Call
+  const handeEndCall = () => {
       dispatch({ type: GLOBAL_TYPES.CALL , payload: null});
       socket.emit('endCall', call)
     }
@@ -62,22 +125,24 @@ const CallModal = () => {
         <></>
       ) : (
         <div className="call_modal">
-          <div className="call_box">
-           <div className="text-center">
-              <Avatar src={call?.avatar} size="avatar-lg" />
-              <h4 className="mt-4">{call?.username}</h4>
+          <div className="call_box"   style={{opacity: (answer && call.video) ? '0' : '1'}}>
+           <div className="text-center call_modal_name">
+             <div className="call_modal_avatar"> <Avatar src={call?.avatar} size="avatar-lg" /></div>
+              <h4 className="">{call?.username}</h4>
               <h6>{call?.fullname}</h6>
                 {
                   answer 
                   ? 
                   <div>
+                      <small>{hours.toString().length < 2 ? '0' +hours : hours}</small>
+                      <small>:</small>
                       <small>{mins.toString().length < 2 ? '0' +mins : mins}</small>
                       <small>:</small>
                       <small>{second.toString().length < 2 ? '0' +second : second}</small>
                   </div>
                   : <div>
                       {call?.video ? (
-                        <span>Đâng gọi video...</span>
+                        <span>Đang gọi video...</span>
                       ) : (
                         <span>Đang gọi điện thoại</span>
                       )}
@@ -85,13 +150,17 @@ const CallModal = () => {
                 }
              
            </div>
-
-            <div className="timer">
-                <small>{mins.toString().length < 2 ? '0' +mins : mins}</small>
-                <small>:</small>
-                <small>{second.toString().length < 2 ? '0' +second : second}</small>
-          </div>
+                {
+                  !answer &&
+                  <div className="timer">
+                    <small>{mins.toString().length < 2 ? '0' +mins : mins}</small>
+                    <small>:</small>
+                    <small>{second.toString().length < 2 ? '0' +second : second}</small>
+                 </div>
+                }
+           
           <div className="call_menu">
+          
                 <span className="material-icons text-danger" onClick={handeEndCall}>call_end</span>
                 {
                   (call.recipient === auth.user._id && !answer) &&
@@ -110,9 +179,27 @@ const CallModal = () => {
                 }
                
           </div>
+             
           </div>
-          
+          <div className="show_video"
+              style={{display: (answer && call.video) ? 'block' : 'none'}}
+              >
+                  <video ref={youVideo} className="you_video" />
+                  <video ref={otherVideo} className="other_video" />
+
+                  <div className="time_video">
+                      <small>{hours.toString().length < 2 ? '0' +hours : hours}</small>
+                      <small>:</small>
+                      <small>{mins.toString().length < 2 ? '0' +mins : mins}</small>
+                      <small>:</small>
+                      <small>{second.toString().length < 2 ? '0' +second : second}</small>
+                  </div>
+
+                  <span className="material-icons text-danger end_call" onClick={handeEndCall}>call_end</span>
+
+              </div>
         </div>
+        
       )}
     </>
   );
