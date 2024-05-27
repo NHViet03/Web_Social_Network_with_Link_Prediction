41e40d54cedd9ed6958b9191fc3ab2bbd1551891
Nguyễn Hoàng Phúc <hoangphucseiza@gmail.com>
2024-05-28 00:09:26 +0700

    Suggestion user API Common Neighbor and Jaccard

---
+++ fastApiServer/schemas/user.py
@@ -0,0 +1,21 @@
+def userEntity(item) -> dict:
+    return {
+        "id": str(item["_id"]),
+        "fullname": item.get("fullname"),
+        "username": item.get("username"),
+        "email": item.get("email"),
+        "password": item.get("password"),
+        "birthday": item.get("birthday"),
+        "avatar": item.get("avatar"),
+        "role": item.get("role"),
+        "gender": item.get("gender"),
+        "story": item.get("story"),
+        "website": item.get("website"),
+        "followers": [str(follower) for follower in item.get("followers", [])],
+        "following": [str(following) for following in item.get("following", [])],
+        "saved": [str(saved) for saved in item.get("saved", [])],
+        "created_at": item.get("created_at"),
+        "updated_at": item.get("updated_at")
+    }
+def usersEntity(items) -> list:
+    return [userEntity(item) for item in items]
